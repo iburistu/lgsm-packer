6a31d70f71b772967449125c18e26b463ef47bfc
iburistu <zachary.linkletter@maine.edu>
2021-06-05 13:00:10 -0400

    Add python3 dependency to games that require it

--- provision_scripts/dstserver.sh
+++ provision_scripts/dstserver.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-sudo apt install -y curl cpio wget file tar bzip2 gzip unzip bsdmainutils python util-linux ca-certificates binutils bc jq tmux netcat lib32gcc1 lib32stdc++6 libsdl2-2.0-0:i386 libcurl4-gnutls-dev:i386
+sudo apt install -y curl cpio wget file tar bzip2 gzip unzip bsdmainutils python python3 util-linux ca-certificates binutils bc jq tmux netcat lib32gcc1 lib32stdc++6 libsdl2-2.0-0:i386 libcurl4-gnutls-dev:i386
--- provision_scripts/mcbserver.sh
+++ provision_scripts/mcbserver.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-sudo apt install -y curl cpio wget file tar bzip2 gzip unzip bsdmainutils python util-linux ca-certificates binutils bc jq tmux netcat lib32gcc1 lib32stdc++6
+sudo apt install -y curl cpio python3 wget file tar bzip2 gzip unzip bsdmainutils python util-linux ca-certificates binutils bc jq tmux netcat lib32gcc1 lib32stdc++6
